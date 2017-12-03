1e7055a804700be2da17849644709b450810c442
Ivan Bedecarats <igbedecarats@gmail.com>
2017-12-03 19:10:23 -0300

    Initial Commit

---
+++ src/main/groovy/app/ui/AppUI.groovy
@@ -0,0 +1,39 @@
+package app.ui
+
+import com.vaadin.annotations.Theme
+import com.vaadin.annotations.Widgetset
+import com.vaadin.server.VaadinRequest
+import com.vaadin.spring.annotation.SpringUI
+import com.vaadin.ui.Button
+import com.vaadin.ui.Label
+import com.vaadin.ui.UI
+import com.vaadin.ui.themes.ValoTheme
+import org.vaadin.viritin.layouts.MVerticalLayout
+
+import java.time.Instant
+
+@SpringUI
+@Theme('app')
+@Widgetset('app')
+class AppUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        def layout = new MVerticalLayout().withSpacing(true).withMargin(true)
+
+        layout.addComponent(new Label('Hello Vaadin').with {
+            setStyleName(ValoTheme.LABEL_H1)
+            it
+        })
+
+        // def editor = new org.vaadin.openesignforms.ckeditor.CKEditorTextField()
+        // layout.addComponent(editor)
+
+        layout.addComponents(new Button('Click for time', {
+            layout.addComponent(new Label("The time on server is ${Instant.now()}"))
+        } as Button.ClickListener))
+
+        setContent(layout)
+    }
+
+}
